0a461b27fd6833fca64e48119eec166b546cdcb6
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-16 07:44:04 -0500

    Update mfp.h

--- mfp.h
+++ mfp.h
@@ -4,6 +4,18 @@
 #ifndef MFP_H
 #define MFP_H
 
+#define MFP_VERSION 1
+
+#include <string>
+
+class MFP {
+public:
+  MFP();
+  ~MFP();
+
+void senderMap(const std::string& _name);
+
+};
 
 
 #endif
